2ac617c404fc1b44f9f51c934b5342c3bed656a0
XainHUN <30698354+XainHUN@users.noreply.github.com>
2017-10-06 16:19:25 +0200

    Add files via upload

---
+++ enum/UserRoleEnum.js
@@ -0,0 +1,13 @@
+﻿function define(name, value) {
+    Object.defineProperty(exports, name, {
+        value: value,
+        enumerable: true,
+        writable: true,
+        configurable: true
+    });
+}
+
+define("ADMIN", 1);
+define("USER", 2);
+define("MODER", 1);
+define("GUEST", 3);
